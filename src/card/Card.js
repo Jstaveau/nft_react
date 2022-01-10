9fdb0907bc53d6cc61df756419c39cef6d13f4e0
jonathan <jonathan.staveau@outlook.com>
2022-01-10 22:09:05 +0100

    js image ok

---
+++ src/card/Card.js
@@ -0,0 +1,12 @@
+import "./Card.css";
+import ImgCard from "../imgCard/ImgCard.js";
+
+function Card() {
+    return (
+    <div className="contourCard">
+        <ImgCard />
+    </div>
+    )
+}
+
+export default Card
